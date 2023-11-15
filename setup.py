3c1f87fcb7acedcbc1aa7de3c47ec02b4ca4a709
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-11-15 11:41:53 +0500

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='Student_Performance_Indicator',
+    version='0.0.1',
+    author='wasiq',
+    author_email='wasiqabdullah222@gmail.com',
+    packages=find_packages(),
+    install_requires=['pandas', 'numpy', 'seaborn']
+)
