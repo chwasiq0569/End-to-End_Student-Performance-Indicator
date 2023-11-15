2b648f4294dac137caaac044a0bee8162589a0c3
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-11-15 12:39:39 +0500

    Created Package through setup.pu

--- setup.py
+++ setup.py
@@ -1,4 +1,16 @@
 from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
 
 setup(
     name='Student_Performance_Indicator',
@@ -6,5 +18,5 @@ setup(
     author='wasiq',
     author_email='wasiqabdullah222@gmail.com',
     packages=find_packages(),
-    install_requires=['pandas', 'numpy', 'seaborn']
+    install_requires=get_requirements('./requirements.txt')
 )
