93b7059cfe3c7f5fe3eb3de1d3891c27aa446b52
wasiq abdullah <wasiqabdullah222@gmail.com>
2023-12-02 17:02:18 +0500

    Created Prediction Pipeline

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -1,18 +1,24 @@
 import os
 import sys
+from src.exception import CustomException
+from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
-from src.logger import logging
-from src.exception import CustomException
+
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
+from src.components.model_trainer import ModelTrainerConfig
 from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
 class DataIngestionConfig:
-    train_data_path = str = os.path.join('artifacts', 'train.csv')
-    test_data_path = str = os.path.join('artifacts', 'test.csv')
-    raw_data_path = str = os.path.join('artifacts', 'data.csv')
+    train_data_path: str = os.path.join('artifacts', "train.csv")
+    test_data_path: str = os.path.join('artifacts', "test.csv")
+    raw_data_path: str = os.path.join('artifacts', "data.csv")
 
 
 class DataIngestion:
@@ -20,30 +26,33 @@ class DataIngestion:
         self.ingestion_config = DataIngestionConfig()
 
     def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
-            data = pd.read_csv("notebook\data\data.csv")
-            logging.info("Read the Dataset as Dataframe")
+            df = pd.read_csv('notebook\data\stud.csv')
+            logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(
                 self.ingestion_config.train_data_path), exist_ok=True)
 
-            data.to_csv(self.ingestion_config.raw_data_path,
-                        index=False, header=True)
+            df.to_csv(self.ingestion_config.raw_data_path,
+                      index=False, header=True)
 
-            logging.info("Train Test Split Initiated...")
+            logging.info("Train test split initiated")
             train_set, test_set = train_test_split(
-                data, test_size=0.2, random_state=42)
+                df, test_size=0.2, random_state=42)
 
             train_set.to_csv(
                 self.ingestion_config.train_data_path, index=False, header=True)
 
             test_set.to_csv(self.ingestion_config.test_data_path,
                             index=False, header=True)
-            logging.info("Data Ingestion Completed")
+
+            logging.info("Inmgestion of the data iss completed")
 
             return (
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
+
             )
         except Exception as e:
             raise CustomException(e, sys)
@@ -53,10 +62,10 @@ if __name__ == "__main__":
     obj = DataIngestion()
     train_data, test_data = obj.initiate_data_ingestion()
 
-    from src.components.data_transformation import DataTransformation
-
     data_transformation = DataTransformation()
-    train_arr, test_arr, preprocessor_obj_file_path = data_transformation.initiate_data_transformation(
+    train_arr, test_arr, _ = data_transformation.initiate_data_transformation(
         train_data, test_data)
-    model_trainer = ModelTrainer()
-    print(model_trainer.initiate_model_trainer(train_arr, test_arr))
+
+    modeltrainer = ModelTrainer()
+    print("MODEL TRAINER")
+    print(modeltrainer.initiate_model_trainer(train_arr, test_arr))
--- src/exception.py
+++ src/exception.py
@@ -9,6 +9,7 @@ def error_message_detail(error,error_detail:sys):
 
     return error_message
 
+    
 
 class CustomException(Exception):
     def __init__(self,error_message,error_detail:sys):
@@ -17,10 +18,7 @@ class CustomException(Exception):
     
     def __str__(self):
         return self.error_message
+    
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Dividing By Zero")
-        raise CustomException(e, sys)
+
+        
